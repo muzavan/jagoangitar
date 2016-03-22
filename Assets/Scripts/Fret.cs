ed0a2f16663e968a24dc11e082b899ffdc4231da
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-03-23 00:54:28 +0700

    Layout Finished. Scripts need to Be Added

---
+++ Assets/Scripts/Fret.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fret : MonoBehaviour {
+
+	public enum State
+	{
+		NotActive,
+		Incorrect,
+		Correct
+	};
+	public int fretNumber = 0;
+	public int stringNumber = 1;
+	private int yLoc = 0;
+	private double frequency;
+	private State state = State.NotActive;
+	private readonly double[] nilFreq = {82.40 , 110.00 , 146.80, 196.00, 246.90,329.60}; // Frekuensi dasar tiap senar
+	private const double freqDif = 6.00; //karena beda tiap fret 6, jadi rentang setiap nada +- 3
+
+	// Use this for initialization
+	void Start () {
+		yLoc = stringNumber * (-15);
+		this.gameObject.transform.position.Set (this.gameObject.transform.position.x,((float)stringNumber*15),this.gameObject.transform.position.z);
+		frequency = nilFreq [stringNumber - 1] + (double)(fretNumber) * (freqDif);
+		Image button = this.GetComponent<Image> ();
+		button.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(state == State.Correct){
+			//ubah warna jadi hijau
+		}
+			
+	}
+
+	void checkFret(double freq){
+		if (Mathf.Abs (Mathf.RoundToInt((float)(freq - frequency))) <= (freqDif / 2)) {
+			state = State.Correct;
+		}
+	}
+
+
+}
