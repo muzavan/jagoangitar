42a94ad8471d75ab27514c4c5415886f5c0c1b6e
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-05-10 17:22:11 +0700

    Added Sound for Every Button

--- Assets/Scripts/SceneSelector.cs
+++ Assets/Scripts/SceneSelector.cs
@@ -5,6 +5,7 @@ using System.Threading;
 public class SceneSelector : MonoBehaviour {
 
 	public int thisScreenId = 0;
+	public AudioSource aSource;
 	// Use this for initialization
 	void Start () {
 		
@@ -16,6 +17,12 @@ public class SceneSelector : MonoBehaviour {
 	}
 
 	public void ScreenSelect(int screenIndex){
+		aSource.Play ();
+		StartCoroutine (DelayStop(screenIndex));
+	}
+
+	IEnumerator DelayStop(int screenIndex){
+		yield return new WaitForSeconds (1);
 		Debug.Log ("ScreenSelect("+screenIndex+")");
 		UnityEngine.SceneManagement.SceneManager.LoadScene (screenIndex);
 		UnityEngine.SceneManagement.SceneManager.UnloadScene (thisScreenId);
